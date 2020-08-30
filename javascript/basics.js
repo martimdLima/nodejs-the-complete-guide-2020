d4dc629da791056f2eb1f457e9157e025a8b175f
Martim Lima <martim.d.lima@protonmail.com>
2020-08-31 00:11:10 +0100

    Javascript
    - Arrays & Array Methods

--- javascript/basics.js
+++ javascript/basics.js
@@ -11,26 +11,38 @@ const sumarizeUser = (userName, userAge, userhasHobby) => {
     ", is hobbies are: " +
     userhasHobby
   );
-}
-
-const add = (a,b) => a + b;
-const add1 = a => a + 1;
-const addRandom = () => 3 * 3.14;
-
-const person = {
-    name: 'user',
-    age: 29,
-    greet() {
-        console.log('My name is: ' + this.name);
-    }
 };
 
-
-
 console.log(sumarizeUser(name, age, hasHobbies));
+
+const add = (a, b) => a + b;
+const add1 = (a) => a + 1;
+const addRandom = () => 3 * 3.14;
 
 console.log(add(1, 2));
 console.log(add1(2));
 console.log(addRandom());
 
+const person = {
+  name: "user",
+  age: 29,
+  greet() {
+    console.log("My name is: " + this.name);
+  },
+};
+
 person.greet();
+
+const hobbies = ["Sports", "Cooking"];
+
+for (let hobby of hobbies) {
+  console.log(hobby);
+}
+
+console.log(
+  hobbies.map((hobby) => {
+    return "Hobby: " + hobby;
+  })
+);
+
+console.log(hobbies.map((hobby) => "Hobby: " + hobby));
