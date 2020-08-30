459f60490ca705fadf920a08ad5f82ed586b3fb0
Martim Lima <martim.d.lima@protonmail.com>
2020-08-31 00:05:29 +0100

    javascript
    -  Understanding Arrow Functions
    -  Working with Objects, Properties & Methods

--- javascript/basics.js
+++ javascript/basics.js
@@ -2,7 +2,7 @@ const name = "Max";
 let age = 29;
 const hasHobbies = true;
 
-function sumarizeUser(userName, userAge, userhasHobby) {
+const sumarizeUser = (userName, userAge, userhasHobby) => {
   return (
     "Name is " +
     userName +
@@ -13,4 +13,24 @@ function sumarizeUser(userName, userAge, userhasHobby) {
   );
 }
 
+const add = (a,b) => a + b;
+const add1 = a => a + 1;
+const addRandom = () => 3 * 3.14;
+
+const person = {
+    name: 'user',
+    age: 29,
+    greet() {
+        console.log('My name is: ' + this.name);
+    }
+};
+
+
+
 console.log(sumarizeUser(name, age, hasHobbies));
+
+console.log(add(1, 2));
+console.log(add1(2));
+console.log(addRandom());
+
+person.greet();
