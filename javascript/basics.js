6a5c235ada0a6918c08499da2be45315ee9cce04
Martim Lima <martim.d.lima@protonmail.com>
2020-08-30 23:49:32 +0100

    Javascript
    - Refreshing the Core Syntax

---
+++ javascript/basics.js
@@ -0,0 +1,9 @@
+var name = 'Max',
+var age = 29;
+var hasHobbies = true;
+
+function sumarizeUser(userName, userAge, userhasHobby) {
+    return 'Name is ' + userName + ', is age is ' + userAge + ', is hobbies are: ' + userhasHobby;
+}
+
+console.log(sumarizeUser(name, age, hasHobbies));
