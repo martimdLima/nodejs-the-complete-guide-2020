0c3c7d92990791c451a45c183129b01ca9d8e11d
Martim Lima <martim.d.lima@protonmail.com>
2020-08-31 01:59:47 +0100

    Basics
    - Understanding Requests

--- basics/app.js
+++ basics/app.js
@@ -1,14 +1,18 @@
 const http = require("http");
 
 const server = http.createServer((request, response) => {
-  console.log(request);
+  
+    console.log(request);
 
-    /*   
+    console.log(request.url, request.method, request.headers);
+
+    /*
+    process.exit();
+
     Process.exit basically hard exites the event loop and therefore the program shuts down because there is no more work to do,
     nodejs sees that there is no more work to do and it basically closes the program and gives control back to the terminal. 
     */
-   
-  //process.exit();
+
 });
 
 server.listen(3000);
