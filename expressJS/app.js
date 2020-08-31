ea1a589ea53659927aad0334aa567c0c0c4c20e0
Martim Lima <martim.d.lima@protonmail.com>
2020-08-31 22:12:06 +0100

    Express.js
    - Using Express Router

--- expressJS/app.js
+++ expressJS/app.js
@@ -1,28 +1,14 @@
-const http = require("http");
 const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/add-product", (req, res, next) => {
-  console.log("In another Middleware!");
-  res.send(
-    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add a product</button></form>'
-  );
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
-app.use("/product", (req, res, next) => {
-  console.log(req.body);
-
-  res.redirect("/");
-});
-
-app.use("/", (req, res, next) => {
-  console.log("In another Middleware!");
-  res.send("<h1>Hello from Express.js</h1>");
-});
-
-const server = http.createServer(app);
-server.listen(3000);
+app.listen(3000);
