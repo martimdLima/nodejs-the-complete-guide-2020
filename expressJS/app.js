9bbda465b97ddacb4b860391fc8adb1bff8fd027
Martim Lima <martim.d.lima@protonmail.com>
2020-08-31 19:56:50 +0100

    Express.js
    - How Middleware Works

--- expressJS/app.js
+++ expressJS/app.js
@@ -6,11 +6,12 @@ const app = express();
 
 app.use((req, res, next) => {
     console.log("In the Middleware!");
-    next();
+    next(); // allows the request to continue to the next middleware in the line
 });
 
 app.use((req, res, next) => {
     console.log("In another Middleware!");
+    res.send("<h1>Hello from Express.js</h1>");
 });
 
 const server = http.createServer(app);
