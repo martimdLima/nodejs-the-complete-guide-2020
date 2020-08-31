c8fa432dcb23bfb5b5725557d2bdc8cd2d469a04
Martim Lima <martim.d.lima@protonmail.com>
2020-08-31 01:54:24 +0100

    Basics
    - The Node Lifecycle & Event Loop

--- basics/app.js
+++ basics/app.js
@@ -1,8 +1,14 @@
-const http = require('http');
-
+const http = require("http");
 
 const server = http.createServer((request, response) => {
-    console.log(request);
+  console.log(request);
+
+    /*   
+    Process.exit basically hard exites the event loop and therefore the program shuts down because there is no more work to do,
+    nodejs sees that there is no more work to do and it basically closes the program and gives control back to the terminal. 
+    */
+   
+  //process.exit();
 });
 
 server.listen(3000);
