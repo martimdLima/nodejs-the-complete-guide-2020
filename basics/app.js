fcd0df088e110aadff256ccb525b8c35e8a5194f
Martim Lima <martim.d.lima@protonmail.com>
2020-08-31 02:06:26 +0100

    Basics
    - Sending Responses

--- basics/app.js
+++ basics/app.js
@@ -4,7 +4,16 @@ const server = http.createServer((request, response) => {
   
     console.log(request);
 
+    // Access the information of the request
     console.log(request.url, request.method, request.headers);
+
+    // Sending Responses
+    response.setHeader('Content-Type', 'text/html');
+    response.write('<html>');
+    response.write('<head><title>Test Page</title><head>');
+    response.write('<body><h1>NodeJs Test Content</h1></body>');
+    response.write('</html>');
+    response.end();
 
     /*
     process.exit();
@@ -12,7 +21,6 @@ const server = http.createServer((request, response) => {
     Process.exit basically hard exites the event loop and therefore the program shuts down because there is no more work to do,
     nodejs sees that there is no more work to do and it basically closes the program and gives control back to the terminal. 
     */
-
 });
 
 server.listen(3000);
