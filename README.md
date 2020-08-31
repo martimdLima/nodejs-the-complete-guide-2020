16928df2e0be0123cb6272f14912f4a0d6582781
Martim Lima <martim.d.lima@protonmail.com>
2020-08-31 20:13:19 +0100

    Express.js
    - Handling Different Routes

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Nodejs The Complete Guide 2020
 
 Complete guide about Node JS & Deno.js, build REST APIs with Node.js, GraphQL APIs, add Authentication, use MongoDB, SQL & much more.
+
+# Modules:
+### 1 - Introduction
+### 2 - Javascript - Quick Refresher
+### 3 - Understanding the Basics
+### 4 - Express.Js
--- expressJS/app.js
+++ expressJS/app.js
@@ -1,18 +1,17 @@
-const http = require("http");
-
-const express = require("express");
+import { createServer } from "http";
+import { express } from "express";
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log("In the Middleware!");
-    next(); // allows the request to continue to the next middleware in the line
+app.use('/add-product', (req, res, next) => {
+    console.log("In another Middleware!");
+    res.send("<h1>The Add Product Page </h1>");
 });
 
-app.use((req, res, next) => {
+app.use('/', (req, res, next) => {
     console.log("In another Middleware!");
     res.send("<h1>Hello from Express.js</h1>");
 });
 
-const server = http.createServer(app);
+const server = createServer(app);
 server.listen(3000);
