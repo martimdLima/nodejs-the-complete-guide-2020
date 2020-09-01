6b2dfd964a48adf31c87f18d569596f720b70c2e
Martim Lima <martim.d.lima@protonmail.com>
2020-09-01 14:34:04 +0100

    Working with Dynamic Content & Adding Templating Engines
    - Sharing Data Across Requests & Users

---
+++ dynamic-content-and-templating-engines/routes/shop.js
@@ -0,0 +1,15 @@
+const path = require("path");
+const rootDir = require("../utils/path");
+
+const express = require("express");
+
+const router = express.Router();
+
+const adminData = require('./admin');
+
+router.get("/", (req, res, next) => {
+  console.log(adminData.products);
+  res.sendFile(path.join(rootDir, "views", "shop.html"));
+});
+
+module.exports = router;
