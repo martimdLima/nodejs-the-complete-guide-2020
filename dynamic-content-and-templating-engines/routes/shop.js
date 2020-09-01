b9c7ba748ca73c05f385577aae890d515bd2ef5e
Martim Lima <martim.d.lima@protonmail.com>
2020-09-01 23:40:24 +0100

    Working with Dynamic Content & Adding Templating Engines
    - Adding the Layout to Handlebars

--- dynamic-content-and-templating-engines/routes/shop.js
+++ dynamic-content-and-templating-engines/routes/shop.js
@@ -5,16 +5,23 @@ const express = require("express");
 
 const router = express.Router();
 
-const adminData = require('./admin');
+const adminData = require("./admin");
 
 router.get("/", (req, res, next) => {
-/* 
+  /* 
   console.log(adminData.products);
   res.sendFile(path.join(rootDir, "views", "shop.html")); 
 */
 
   const products = adminData.products;
-  res.render('shop', {prods: products, pageTitle: 'Shop', path:"/", hasProducts: products.length > 0});
+  res.render("shop", {
+    prods: products,
+    pageTitle: "Shop",
+    path: "/",
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true,
+  });
 });
 
 module.exports = router;
