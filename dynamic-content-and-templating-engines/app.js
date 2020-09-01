3925e89a4483a67fbd0953173a47a8bbe3d585a3
Martim Lima <martim.d.lima@protonmail.com>
2020-09-01 20:33:40 +0100

    Working with Dynamic Content & Adding Templating Engines
    - Working with Handlebars

--- dynamic-content-and-templating-engines/app.js
+++ dynamic-content-and-templating-engines/app.js
@@ -2,11 +2,13 @@ const path = require("path");
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const expressHbs = require("express-handlebars");
 
 const app = express();
 
-app.set('view engine', 'pug');
-app.set('views', 'views');
+app.engine('hbs', expressHbs ({extname: 'hbs',defaultLayout: null}));
+app.set("view engine", "hbs");
+app.set("views", "views");
 
 const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
@@ -18,9 +20,9 @@ app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, nex) => {
-  //   res.status(404).send("<h1>page Not Found</h1>");
+  // res.status(404).send("<h1>page Not Found</h1>");
   // res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
-  res.render('404', { pageTitle: '404 - Page Not Found' });
+  res.render("404", { pageTitle: "404 - Page Not Found" });
 });
 
 app.listen(3000);
