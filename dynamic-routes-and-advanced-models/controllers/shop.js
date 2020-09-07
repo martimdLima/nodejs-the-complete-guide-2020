6dbc59c946662c4bee4c2682e811211991e6d63f
Martim Lima <martim.d.lima@protonmail.com>
2020-09-07 22:36:32 +0100

    Dynamic Routes And Advanced Models
    - Loading Product Detail Data

--- dynamic-routes-and-advanced-models/controllers/shop.js
+++ dynamic-routes-and-advanced-models/controllers/shop.js
@@ -12,7 +12,9 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  console.log(prodId);
+  Product.fetchById(prodId, (product) => {
+    console.log(product);
+  });
   res.redirect("/");
 };
 
