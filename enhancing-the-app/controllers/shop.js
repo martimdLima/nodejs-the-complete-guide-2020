7a0963d51f32987dd1a706b6a7d3ae12bff715d8
Martim Lima <martim.d.lima@protonmail.com>
2020-09-07 20:15:58 +0100

    Enhancing the App
    - Storing Product Data

--- enhancing-the-app/controllers/shop.js
+++ enhancing-the-app/controllers/shop.js
@@ -1,6 +1,5 @@
 const Product = require("../models/product");
 
-
 exports.getProducts = (req, res, next) => {
   const products = Product.fetchAll((products) => {
     res.render("shop/product-list", {
@@ -12,25 +11,25 @@ exports.getProducts = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-    Product.fetchAll((products) => {
-        res.render("shop/index", {
-          prods: products,
-          pageTitle: "Shop",
-          path: "/",
-        });
-      });
+  Product.fetchAll((products) => {
+    res.render("shop/index", {
+      prods: products,
+      pageTitle: "Shop",
+      path: "/",
+    });
+  });
 };
 
 exports.getCart = (req, res, next) => {
-    res.render("shop/cart", {
-            path:"/cart",
-            pageTitle: "Your Cart"
-        })
-}
+  res.render("shop/cart", {
+    path: "/cart",
+    pageTitle: "Your Cart",
+  });
+};
 
 exports.getCheckout = (req, res, next) => {
-    res.render("shop/checkout", {
-        path: "checkout",
-        pageTitle: "Checkout"
-    })
-}
+  res.render("shop/checkout", {
+    path: "checkout",
+    pageTitle: "Checkout",
+  });
+};
