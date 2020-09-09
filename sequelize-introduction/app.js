f59dff8404e826c7a86705ba163cb79be3085947
Martim Lima <martim.d.lima@protonmail.com>
2020-09-09 16:29:55 +0100

    Sequelize
    - Creating & Managing a Dummy User

--- sequelize-introduction/app.js
+++ sequelize-introduction/app.js
@@ -12,14 +12,25 @@ const User = require("./models/user");
 
 const app = express();
 
-app.set("view engine", "ejs");
-app.set("views", "views");
-
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
+app.set("view engine", "ejs");
+app.set("views", "views");
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
@@ -30,9 +41,26 @@ Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
 User.hasMany(Product);
 
 sequelize
-  .sync({ force: true })
+  //.sync({force: true})
+  .sync()
   .then((result) => {
+    return User.findByPk(1);
     //console.log(result);
+    //app.listen(3000);
+  })
+  .then((user) => {
+    if (!user) {
+      return User.create({
+        name: "testUser",
+        email: "test@test.com",
+      });
+    } else {
+      //return Promise.resolve(user);
+      return user;
+    }
+  })
+  .then((user) => {
+    //console.log(user);
     app.listen(3000);
   })
   .catch((err) => {
--- sequelize-introduction/models/user.js
+++ sequelize-introduction/models/user.js
@@ -3,20 +3,20 @@ const Sequelize = require("sequelize");
 const sequelize = require("../util/database");
 
 const User = sequelize.define("user", {
-    id: {
-      type: Sequelize.INTEGER,
-      autoIncrement: true,
-      allowNull: false,
-      primaryKey: true,
-    },
-    name: {
-      type: Sequelize.STRING,
-      allowNull: false,
-    },
-    email: {
-      type: Sequelize.STRING,
-      allowNull: false,
-    },
-  });
-  
-  module.exports = User;
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+});
+
+module.exports = User;
