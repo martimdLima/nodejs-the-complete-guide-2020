d1d6433df9d75ded12c7cd387e6f718d44fff419
Martim Lima <martim.d.lima@protonmail.com>
2020-09-09 20:15:24 +0100

    Sequelize
    - Deleting Related Items & Deleting Cart Products

--- sequelize-introduction/controllers/shop.js
+++ sequelize-introduction/controllers/shop.js
@@ -108,10 +108,22 @@ exports.postCart = (req, res, next) => {
 
 exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.fetchById(prodId, (product) => {
-    Cart.deleteProduct(prodId, product.price);
-    res.redirect("/cart");
-  });
+
+  req.user
+    .getCart()
+    .then((cart) => {
+      return cart.getProducts({ where: { id: prodId } });
+    })
+    .then((products) => {
+      const product = products[0];
+      return product.cartItem.destroy();
+    })
+    .then((result) => {
+      res.redirect("/cart");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
 exports.getCheckout = (req, res, next) => {
