ec0b54111e10374150461f033c1ab35766c96581
Martim Lima <martim.d.lima@protonmail.com>
2020-09-10 16:12:26 +0100

    NoSQL & MongoDB
    - Creating New Users

--- nosql-introduction/app.js
+++ nosql-introduction/app.js
@@ -6,6 +6,8 @@ const bodyParser = require("body-parser");
 const errorController = require("./controllers/error");
 const mongoConnect = require("./util/database").mongoConnect;
 
+const User = require("./models/user");
+
 const app = express();
 
 const adminRoutes = require("./routes/admin");
@@ -18,14 +20,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
-  /*   User.findByPk(1)
+    User.findById("5f5a416373b1c427392a5208")
     .then((user) => {
       req.user = user;
       next();
     })
     .catch((err) => {
       console.log(err);
-    }); */
+    });
   next();
 });
 
--- nosql-introduction/models/user.js
+++ nosql-introduction/models/user.js
@@ -1,4 +1,4 @@
-const Sequelize = require("sequelize");
+/* const Sequelize = require("sequelize");
 
 const sequelize = require("../util/database");
 
@@ -17,6 +17,34 @@ const User = sequelize.define("user", {
     type: Sequelize.STRING,
     allowNull: false,
   },
-});
+}); */
+
+const { getDb } = require("../util/database");
+const mongodb = require("mongodb");
+
+const ObjectId = mongodb.ObjectID;
+
+const db = require("../util/database").getDb;
+
+class User {
+  constructor(username, email) {
+    this.username = username;
+    this.email = email;
+    //this._id = id ? new mongodb.ObjectId(id) : null;
+  }
+
+  save() {
+    const db = getDb();
+    return db.collection("users").insertOne(this);
+  }
+
+  static findById(userId) {
+    const db = getDb();
+    return db.collection("users").findOne({_id: new ObjectId(userId)});
+  }
+
+
+
+}
 
 module.exports = User;
