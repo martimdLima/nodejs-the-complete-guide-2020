6eae3ec5aa3a374ca8d30f63748010a0bc045835
Martim Lima <martim.d.lima@protonmail.com>
2020-09-11 15:37:20 +0100

    Session & Cookies
    - Configuring Cookies

--- sessions-and-cookies/controllers/auth.js
+++ sessions-and-cookies/controllers/auth.js
@@ -1,18 +1,17 @@
 exports.getLogin = (req, res, next) => {
+  //const isLoggedIn = req.get("Cookie").trim().split("=")[1] === true;
 
-    const isLoggedIn = req.get("Cookie").trim().split("=")[1] === true;
-
-    res.render("auth/login", {
-      path: "/login",
-      pageTitle: "Login Page",
-      isAuthenticated: isLoggedIn,
-    });
+  res.render("auth/login", {
+    path: "/login",
+    pageTitle: "Login Page",
+    isAuthenticated: false,
+  });
 };
 
 exports.postLogin = (req, res, next) => {
   //req.isLoggedIn = true;
-
-  res.setHeader("Set-Cookie", "loggedIn=true");
+  // Cookie properties - Expires | Max-Age | Domain | Secure
+  res.setHeader("Set-Cookie", "loggedIn=true; HttpOnly");
 
   res.redirect("/");
 };
