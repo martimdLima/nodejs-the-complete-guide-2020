91dd33f8ee118025519fb70c20c132ae02b8bfd7
Martim Lima <martim.d.lima@protonmail.com>
2020-09-11 01:07:44 +0100

    mongoose
    - Saving Data Through Mongoose

--- mongoose-introduction/app.js
+++ mongoose-introduction/app.js
@@ -7,7 +7,7 @@ const mongoose = require("mongoose");
 
 const errorController = require("./controllers/error");
 
-const User = require("./models/user");
+//const User = require("./models/user");
 
 const app = express();
 
@@ -20,7 +20,7 @@ app.set("views", "views");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use((req, res, next) => {
+/* app.use((req, res, next) => {
   User.findById("5f5a416373b1c427392a5208")
     .then((user) => {
       req.user = new User(user.name, user.email, user.cart, user._id);
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
     .catch((err) => {
       console.log(err);
     });
-});
+}); */
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
@@ -38,7 +38,7 @@ app.use(errorController.get404);
 
  mongoose
    .connect(
-     "mongodb+srv://mdlima:Fp53UihfDIOC0o7a@cluster0.xmtoh.mongodb.net/test?retryWrites=true&w=majority"
+     "mongodb+srv://mdlima:Fp53UihfDIOC0o7a@cluster0.xmtoh.mongodb.net/shop?retryWrites=true&w=majority"
    )
    .then((result) => {
     console.log("CONNECTED!!!!");
--- mongoose-introduction/routes/admin.js
+++ mongoose-introduction/routes/admin.js
@@ -6,9 +6,8 @@ const router = express.Router();
 
 const adminController = require("../controllers/admin");
 
-/* 
 // /admin/products
-router.get("/products", adminController.getProducts);
+// router.get("/products", adminController.getProducts);
 
 // /admin/add-product => GET
 router.get("/add-product", adminController.getAddProduct);
@@ -16,10 +15,11 @@ router.get("/add-product", adminController.getAddProduct);
 // /admin/add-product => POST
 router.post("/add-product", adminController.postAddProduct);
 
-router.get("/edit-product/:productId", adminController.getEditProduct);
+// router.get("/edit-product/:productId", adminController.getEditProduct);
 
-router.post("/edit-product", adminController.postEditProduct);
+// router.post("/edit-product", adminController.postEditProduct);
 
-router.post("/delete-product", adminController.postDeleteProduct); */
+// router.post("/delete-product", adminController.postDeleteProduct); */
+
 
 module.exports = router;
