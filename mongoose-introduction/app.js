b34462c92159323f2e60d8cc38d9e87484a1d9e8
Martim Lima <martim.d.lima@protonmail.com>
2020-09-11 02:31:07 +0100

    Mongoose
    - Adding and Using a User Model

--- mongoose-introduction/app.js
+++ mongoose-introduction/app.js
@@ -7,7 +7,7 @@ const mongoose = require("mongoose");
 
 const errorController = require("./controllers/error");
 
-//const User = require("./models/user");
+const User = require("./models/user");
 
 const app = express();
 
@@ -20,30 +20,44 @@ app.set("views", "views");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-/* app.use((req, res, next) => {
-  User.findById("5f5a416373b1c427392a5208")
+app.use((req, res, next) => {
+  User.findById("5f5ad2ed27949d5bf02c878b")
     .then((user) => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
+      req.user = user;
       next();
     })
     .catch((err) => {
       console.log(err);
     });
-}); */
+});
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
- mongoose
-   .connect(
-     "mongodb+srv://mdlima:Fp53UihfDIOC0o7a@cluster0.xmtoh.mongodb.net/shop?retryWrites=true&w=majority"
-   )
-   .then((result) => {
+mongoose
+  .connect(
+    "mongodb+srv://mdlima:Fp53UihfDIOC0o7a@cluster0.xmtoh.mongodb.net/shop?retryWrites=true&w=majority"
+  )
+  .then((result) => {
     console.log("CONNECTED!!!!");
-     app.listen(3000);
-   })
-   .catch((err) => {
-     console.log(err);
-   });
+
+    User.findOne().then((user) => {
+      if (!user) {
+        const user = new User({
+          name: "mdLima",
+          email: "mdLima@test.com",
+          cart: {
+            items: [],
+          },
+        });
+        user.save();
+      }
+    });
+
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
--- mongoose-introduction/models/user.js
+++ mongoose-introduction/models/user.js
@@ -1,3 +1,28 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  cart: {
+    items: [
+      {
+        productId: { type: mongoose.SchemaTypes.ObjectId, required: true },
+        quantity: { type: Number, require: true },
+      },
+    ],
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
+
 /* const { getDb } = require("../util/database");
 const mongodb = require("mongodb");
 
