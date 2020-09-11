5b9e0578ecf4ace212d27a4fabdf35a607c5f57d
Martim Lima <martim.d.lima@protonmail.com>
2020-09-11 01:15:22 +0100

    Mongoose
    - Fetching a Single Product

--- mongoose-introduction/routes/shop.js
+++ mongoose-introduction/routes/shop.js
@@ -10,10 +10,9 @@ router.get("/", shopController.getIndex);
 
 router.get("/products", shopController.getProducts);
 
-/* 
-
-
 router.get("/products/:productId", shopController.getProduct);
+
+/* 
 
 router.get("/cart", shopController.getCart);
 
@@ -25,6 +24,6 @@ router.post("/create-order", shopController.postOrder);
 
 router.get("/orders", shopController.getOrders); */
 
-// router.get("/checkout", shopController.getCheckout); 
+// router.get("/checkout", shopController.getCheckout);
 
 module.exports = router;
