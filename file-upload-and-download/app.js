fe7f4a54fa879ac856443a023032872fa1862025
Martim Lima <martim.d.lima@protonmail.com>
2020-09-16 22:30:40 +0100

    FIle Upload And Download
    - Configuring Multer to Adjust Filename & Filepath

--- file-upload-and-download/app.js
+++ file-upload-and-download/app.js
@@ -23,12 +23,20 @@ const store = new MongoDBStore({
   collection: "sessions",
 });
 const csrfProtection = csrf();
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().toISOString() + "-" + file.originalname)
+  }
+});
 
 app.set("view engine", "ejs");
 app.set("views", "views");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(multer({ dest: "./images" }).single("image"));
+app.use(multer({ storage: fileStorage }).single("image"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(
   session({
