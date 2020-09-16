b51304a08d102e0a691c8b74b9a7d44f69c8990e
Martim Lima <martim.d.lima@protonmail.com>
2020-09-16 17:58:30 +0100

    Validation
    - Validating Product Editing

--- validation/controllers/admin.js
+++ validation/controllers/admin.js
@@ -24,6 +24,7 @@ exports.getAddProduct = (req, res, next) => {
     editing: false,
     hasError: false,
     errorMessage: null,
+    validationErrors: [],
   });
 };
 
@@ -36,8 +37,8 @@ exports.postAddProduct = (req, res, next) => {
 
   if (!errors.isEmpty()) {
     return res.status(422).render("admin/edit-product", {
-      path: "/admin/edit-product",
       pageTitle: "Add Product",
+      path: "/admin/edit-product",
       editing: false,
       hasError: true,
       product: {
@@ -47,6 +48,7 @@ exports.postAddProduct = (req, res, next) => {
         description: description,
       },
       errorMessage: errors.array()[0].msg,
+      validationErrors: errors.array(),
     });
   }
 
@@ -90,6 +92,7 @@ exports.getEditProduct = (req, res, next) => {
         product: product,
         hasError: false,
         errorMessage: null,
+        validationErrors: [],
       });
     })
     .catch((err) => {
@@ -103,6 +106,25 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDescription = req.body.description;
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).render("admin/edit-product", {
+      pageTitle: "Edit Product",
+      path: "/admin/edit-product",
+      editing: true,
+      hasError: true,
+      product: {
+        title: updatedTitle,
+        price: updatedPrice,
+        imageUrl: updatedImageUrl,
+        description: updatedDescription,
+        _id: prodId,
+      },
+      errorMessage: errors.array()[0].msg,
+      validationErrors: errors.array(),
+    });
+  }
 
   Product.findById(prodId)
     .then((product) => {
--- validation/routes/admin.js
+++ validation/routes/admin.js
@@ -30,7 +30,7 @@ router.get("/edit-product/:productId", isAuth, adminController.getEditProduct);
 router.post(
   "/edit-product",
   [
-    body("title").isAlphanumeric().isLength({ min: 3 }).trim(),
+    body("title").isString().isLength({ min: 3 }).trim(),
     body("imageUrl").isURL(),
     body("price").isFloat(),
     body("description").isLength({ min: 8, max: 400 }).trim(),
