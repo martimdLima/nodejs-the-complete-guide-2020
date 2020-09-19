fbba26065716ed53d56b02d17294b2e43b075ae3
Martim Lima <martim.d.lima@protonmail.com>
2020-09-19 22:28:47 +0100

    Working With Rest API's
    - Added the backend 
    - Fetching Lists of Posts

---
+++ working-with-rest-apis/be/routes/feed.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const feedController = require("../controllers/feed");
+
+const router = express.Router();
+
+// GET /feed/posts
+router.get('/posts', feedController.getPosts);
+
+router.post('/posts', feedController.postPosts);
+
+
+module.exports = router;
