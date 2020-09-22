4dfae70119742135994a4f39df856a7dfc84d853
Martim Lima <martim.d.lima@protonmail.com>
2020-09-22 18:39:34 +0100

    Working With Rest APIs
    - Adding Auth Middleware to All Routes

--- working-with-rest-apis/be/routes/feed.js
+++ working-with-rest-apis/be/routes/feed.js
@@ -12,6 +12,7 @@ router.get("/posts", isAuth ,feedController.getPosts);
 // POST /feed/post
 router.post(
   "/post",
+  isAuth,
   [
     body("title").trim().isLength({ min: 5 }),
     body("content").trim().isLength({ min: 5 }),
@@ -20,11 +21,12 @@ router.post(
 );
 
 // GET /post/:postId
-router.get("/post/:postId", feedController.getPost);
+router.get("/post/:postId",isAuth, feedController.getPost);
 
 // PUT /post/:postId
 router.put(
   "/post/:postId",
+  isAuth,
   [
     body("title").trim().isLength({ min: 5 }),
     body("content").trim().isLength({ min: 5 }),
@@ -32,6 +34,6 @@ router.put(
   feedController.updatePost
 );
 
-router.delete("/post/:postId", feedController.deletePost);
+router.delete("/post/:postId",isAuth, feedController.deletePost);
 
 module.exports = router;
