1d675beee5e3a951d3d51af69d04be7d37600962
Martim Lima <martim.d.lima@protonmail.com>
2020-09-22 18:53:53 +0100

    Working With Rest APIs
    - Connecting Posts & Users

--- working-with-rest-apis/be/controllers/feed.js
+++ working-with-rest-apis/be/controllers/feed.js
@@ -1,9 +1,10 @@
 const fs = require("fs");
 const path = require("path");
 const { validationResult } = require("express-validator");
-const { nextError, throwError} = require("../util/errorhandling");
+const { nextError, throwError } = require("../util/errorhandling");
 
 const Post = require("../models/post");
+const User = require("../models/user");
 
 exports.getPosts = (req, res, next) => {
   const currentPage = req.query.page || 1;
@@ -20,13 +21,11 @@ exports.getPosts = (req, res, next) => {
         .limit(perPage);
     })
     .then((posts) => {
-      res
-        .status(200)
-        .json({
-          message: "Fetched posts successfully.",
-          posts: posts,
-          totalItems: totalItems,
-        });
+      res.status(200).json({
+        message: "Fetched posts successfully.",
+        posts: posts,
+        totalItems: totalItems,
+      });
     })
     .catch((err) => {
       nextError(err, 500);
@@ -47,19 +46,29 @@ exports.createPost = (req, res, next) => {
   const imageUrl = req.file.path;
   const title = req.body.title;
   const content = req.body.content;
+  let creator;
   const post = new Post({
     title: title,
     content: content,
     imageUrl: imageUrl,
-    creator: { name: "user" },
+    creator: req.userId,
   });
 
   post
     .save()
     .then((result) => {
+      return User.findById(req.userId);
+    })
+    .then((user) => {
+      creator = user;
+      user.posts.push(post);
+      return user.save();
+    })
+    .then((result) => {
       res.status(201).json({
         message: "Post created successfully!",
-        post: result,
+        post: post,
+        creator: { _id: creator._id, name: creator.name },
       });
     })
     .catch((err) => {
