397f11f551ace7d7811b9e0f06104ec78b4dd9a0
Martim Lima <martim.d.lima@protonmail.com>
2020-09-23 14:39:22 +0100

    Understanding Websockets & Socket.io
    - Sharing the IO Instance Across Files

--- websockets-and-socket.io/be/app.js
+++ websockets-and-socket.io/be/app.js
@@ -63,7 +63,7 @@ mongoose
   .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then((result) => {
     const server = app.listen(8080);
-    const io = require("socket.io")(server);
+    const io = require("./socket").init(server);
     io.on("connection", socket => {
       console.log("Client connected!");
     });
