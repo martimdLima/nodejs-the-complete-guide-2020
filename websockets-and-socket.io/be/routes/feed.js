4aee8f3c4f4626001c0571a167fe0f1cce62943b
Martim Lima <martim.d.lima@protonmail.com>
2020-09-23 14:05:05 +0100

    Understanding Websockets & Socket.io
    - Project Setup
    - Setting Up Socket.io on the Server

---
+++ websockets-and-socket.io/be/routes/feed.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const { body } = require("express-validator");
+
+const feedController = require("../controllers/feed");
+const isAuth = require("../middleware/is-auth");
+
+const router = express.Router();
+
+// GET /feed/posts
+router.get("/posts", isAuth ,feedController.getPosts);
+
+// POST /feed/post
+router.post(
+  "/post",
+  isAuth,
+  [
+    body("title").trim().isLength({ min: 5 }),
+    body("content").trim().isLength({ min: 5 }),
+  ],
+  feedController.createPost
+);
+
+// GET /post/:postId
+router.get("/post/:postId",isAuth, feedController.getPost);
+
+// PUT /post/:postId
+router.put(
+  "/post/:postId",
+  isAuth,
+  [
+    body("title").trim().isLength({ min: 5 }),
+    body("content").trim().isLength({ min: 5 }),
+  ],
+  feedController.updatePost
+);
+
+router.delete("/post/:postId",isAuth, feedController.deletePost);
+
+module.exports = router;
