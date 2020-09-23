420cc18f4c7526b11142345fec3bb2153ac78c49
Martim Lima <martim.d.lima@protonmail.com>
2020-09-23 16:56:06 +0100

    Understanding Websockets & Socket.io
    - Deleting Posts Across Clients

--- websockets-and-socket.io/be/controllers/feed.js
+++ websockets-and-socket.io/be/controllers/feed.js
@@ -161,6 +161,8 @@ exports.deletePost = async (req, res, next) => {
     user.posts.pull(postId);
     await user.save();
 
+    io.getIO().emit("posts", { action: "delete", post: postId});
+
     res.status(200).json({ message: "Post was deleted" });
   } catch (err) {
     nextError(next, err, 500);
