ac22f9645fd0cbae749c5adb1de01734a2c0c455
Martim Lima <martim.d.lima@protonmail.com>
2020-09-24 03:55:50 +0100

    GraphQL
    - Handling Errors

--- working-with-graphql/be/graphql/resolvers.js
+++ working-with-graphql/be/graphql/resolvers.js
@@ -7,26 +7,29 @@ module.exports = {
   createUser: async function ({ userInput }, req) {
     //   const email = args.userInput.email;
     const errors = [];
+
     if (!validator.isEmail(userInput.email)) {
-      errors.push({ message: "Email is invalid!" });
+      errors.push({ message: "E-Mail is invalid." });
     }
 
     if (
       validator.isEmpty(userInput.password) ||
       !validator.isLength(userInput.password, { min: 5 })
     ) {
-      errors.push({ message: "Password is invalid!" });
+      errors.push({ message: "Password too short!" });
     }
 
     if (errors.length > 0) {
-      const error = new Error("Invalid input");
+      const error = new Error("Invalid input.");
+      error.data = errors;
+      error.code = 422;
       throw error;
     }
 
     const existingUser = await User.findOne({ email: userInput.email });
 
     if (existingUser) {
-      const error = new Error("User exists already!");
+      const error = new Error("User already exists!");
       throw error;
     }
 
