e34cea63718295b4d648c2313f74d615dd974ada
Martim Lima <martim.d.lima@protonmail.com>
2020-09-24 03:39:48 +0100

    GraphQL
    - Adding Input Validation

--- working-with-graphql/be/app.js
+++ working-with-graphql/be/app.js
@@ -6,7 +6,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const multer = require("multer");
-const { graphqlHTTP } = require('express-graphql'); 
+const { graphqlHTTP } = require("express-graphql");
 
 const graphqlSchema = require("./graphql/schema");
 const graphqlResolver = require("./graphql/resolvers");
@@ -49,12 +49,14 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use("/graphql", graphqlHTTP({
-  schema: graphqlSchema,
-  rootValue: graphqlResolver,
-  graphiql: true
-}))
-
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: graphqlSchema,
+    rootValue: graphqlResolver,
+    graphiql: true,
+  })
+);
 
 app.use((error, req, res, next) => {
   console.log(error);
