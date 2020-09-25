9b135197b4378a2ab86d30c123c44f3b43472c8f
Martim Lima <martim.d.lima@protonmail.com>
2020-09-25 02:38:10 +0100

    Testing Node.js Application
    - Setup and Writing a First Test

---
+++ testing-nodejs-applications/util/errorhandling.js
@@ -0,0 +1,19 @@
+module.exports.throwError = function (statusCode, message) {
+  const error = new Error(message);
+  error.statusCode = statusCode;
+  throw error;
+};
+
+module.exports.authThrowError = function (errors, statusCode, message) {
+  const error = new Error(message);
+  error.statusCode = statusCode;
+  error.data = errors.array();
+  throw error;
+};
+
+module.exports.nextError = function (next, error, statusCode) {
+  if (!error.statusCode) {
+    error.statusCode = statusCode;
+  }
+  next(error);
+};
