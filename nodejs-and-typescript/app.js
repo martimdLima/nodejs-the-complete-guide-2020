74f0eae6c39fd1cbf88f5c254b3864a2f56d0e12
Martim Lima <martim.d.lima@protonmail.com>
2020-09-26 16:55:21 +0100

    Configuring TypeScript
    - Configuring TypeScript

--- nodejs-and-typescript/app.js
+++ nodejs-and-typescript/app.js
@@ -1,10 +1,11 @@
-var num1Element = document.getElementById('num1');
-var num2Element = document.getElementById('num2');
-var buttonElement = document.querySelector('button');
+"use strict";
+var num1Element = document.getElementById("num1");
+var num2Element = document.getElementById("num2");
+var buttonElement = document.querySelector("button");
 function sum(num1, num2) {
     return num1 + num2;
 }
-buttonElement.addEventListener('click', function () {
+buttonElement.addEventListener("click", function () {
     var num1 = +num1Element.value;
     var num2 = +num2Element.value;
     var result = sum(num1, num2);
--- nodejs-and-typescript/app.ts
+++ nodejs-and-typescript/app.ts
@@ -1,12 +1,12 @@
-const num1Element = document.getElementById('num1') as HTMLInputElement;
-const num2Element = document.getElementById('num2') as HTMLInputElement;
-const buttonElement = document.querySelector('button');
+const num1Element = document.getElementById("num1") as HTMLInputElement;
+const num2Element = document.getElementById("num2") as HTMLInputElement;
+const buttonElement = document.querySelector("button")!;
 
 function sum(num1: number, num2: number) {
   return num1 + num2;
 }
 
-buttonElement.addEventListener('click', () => {
+buttonElement.addEventListener("click", () => {
   const num1 = +num1Element.value;
   const num2 = +num2Element.value;
   const result = sum(num1, num2);
