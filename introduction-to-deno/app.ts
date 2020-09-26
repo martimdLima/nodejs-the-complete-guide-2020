bc5864f654e81f4c9510ee724fea8a18c58ff8b0
Martim Lima <martim.d.lima@protonmail.com>
2020-09-26 19:47:42 +0100

    Introduction to Deno
    - Using the Runtime API

--- introduction-to-deno/app.ts
+++ introduction-to-deno/app.ts
@@ -1,5 +1,21 @@
-let message: string;
+const text = `
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+Ut morbi tincidunt augue interdum. Morbi tincidunt ornare massa eget egestas purus. 
+Eget nullam non nisi est sit amet facilisis magna etiam. Fermentum odio eu feugiat pretium. In cursus turpis massa tincidunt dui ut. 
+Nisl nisi scelerisque eu ultrices vitae auctor eu augue. 
+Volutpat consequat mauris nunc congue nisi vitae suscipit tellus mauris. 
+Porta non pulvinar neque laoreet suspendisse. Pretium nibh ipsum consequat nisl vel pretium. 
+Ut faucibus pulvinar elementum integer enim neque. Lorem ipsum dolor sit amet consectetur adipiscing elit. 
+Non pulvinar neque laoreet suspendisse interdum consectetur libero id. Arcu non sodales neque sodales ut.
+`;
 
-message = "Dummy text";
+const encoder = new TextEncoder();
+const data = encoder.encode(text);
 
-console.log(message);
+Deno.writeFile("loremipsum.txt", data)
+  .then(() => {
+    console.log("wrote to file successfully!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
