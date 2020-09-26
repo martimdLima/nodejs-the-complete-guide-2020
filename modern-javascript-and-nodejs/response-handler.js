18ac747dfd0f765afd560e42df74a5dbb35146f4
Martim Lima <martim.d.lima@protonmail.com>
2020-09-26 15:42:32 +0100

    Modern Javascript And Node.JS
    - More on ES Modules

--- modern-javascript-and-nodejs/response-handler.js
+++ modern-javascript-and-nodejs/response-handler.js
@@ -1,10 +1,17 @@
 //const fs = require('fs');
-import fs from "fs";
+//import fs from "fs";
+import path, { dirname } from "path";
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const resHandler = (req, res, next) => {
-    fs.readFile('my-page.html', 'utf8', (err, data) => {
+/*     fs.readFile('my-page.html', 'utf8', (err, data) => {
       res.send(data);
-    });
+    }); */
+
+    res.sendFile(path.join(__dirname, "my-page.html"));
 };
 
 export default resHandler;
