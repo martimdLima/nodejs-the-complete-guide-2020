2804e8180aa48b3740ebfc6ff120cfee3f086ae9
Martim Lima <martim.d.lima@protonmail.com>
2020-09-26 17:01:13 +0100

    Nodejs & Typescript
    - Working with Union Types

--- nodejs-and-typescript/app.ts
+++ nodejs-and-typescript/app.ts
@@ -2,15 +2,25 @@ const num1Element = document.getElementById("num1") as HTMLInputElement;
 const num2Element = document.getElementById("num2") as HTMLInputElement;
 const buttonElement = document.querySelector("button")!;
 
-function sum(num1: number, num2: number) {
-  return num1 + num2;
+function sum(num1: number | string, num2: number | string) {
+  if (typeof num1 === "number" && typeof num2 === "number") {
+    return num1 + num2;
+  } else if (typeof num1 === "string" && typeof num2 === "string") {
+    return num1 + num2;
+  } else {
+    return +num1 + +num2;
+  }
 }
 
 buttonElement.addEventListener("click", () => {
   const num1 = +num1Element.value;
   const num2 = +num2Element.value;
+
   const result = sum(num1, num2);
+  const stringResuslt = sum(num1, num2);
+
   console.log(result);
+  console.log(stringResuslt);
 });
 
 //console.log(sum(1, 6));
