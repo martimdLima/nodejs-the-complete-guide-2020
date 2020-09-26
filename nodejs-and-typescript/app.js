e5c8a1f3f107dcf94f187983ca13e13cacbc3a9f
Martim Lima <martim.d.lima@protonmail.com>
2020-09-26 17:13:59 +0100

    Using Object & Array Types
    - Using Object & Array Types

--- nodejs-and-typescript/app.js
+++ nodejs-and-typescript/app.js
@@ -13,12 +13,20 @@ function sum(num1, num2) {
         return +num1 + +num2;
     }
 }
+function printResult(resultObj) {
+    console.log(resultObj.val);
+}
 buttonElement.addEventListener("click", function () {
+    var numResults = [];
+    var textResults = [];
     var num1 = +num1Element.value;
     var num2 = +num2Element.value;
-    var result = sum(num1, num2);
-    var stringResuslt = sum(num1, num2);
-    console.log(result);
-    console.log(stringResuslt);
+    var numResult = sum(num1, num2);
+    numResults.push(numResult);
+    var stringResult = sum(num1, num2);
+    textResults.push(stringResult);
+    console.log(numResults);
+    console.log(textResults);
+    printResult({ val: numResult, timestamp: new Date() });
 });
 //console.log(sum(1, 6));
--- nodejs-and-typescript/app.ts
+++ nodejs-and-typescript/app.ts
@@ -2,6 +2,7 @@ const num1Element = document.getElementById("num1") as HTMLInputElement;
 const num2Element = document.getElementById("num2") as HTMLInputElement;
 const buttonElement = document.querySelector("button")!;
 
+
 function sum(num1: number | string, num2: number | string) {
   if (typeof num1 === "number" && typeof num2 === "number") {
     return num1 + num2;
@@ -12,15 +13,27 @@ function sum(num1: number | string, num2: number | string) {
   }
 }
 
+function printResult(resultObj: { val: number; timestamp: Date }) {
+  console.log(resultObj.val);
+}
+
 buttonElement.addEventListener("click", () => {
+
+  const numResults: number[] = [];
+  const textResults: string[] = [];  
+
   const num1 = +num1Element.value;
   const num2 = +num2Element.value;
 
-  const result = sum(num1, num2);
-  const stringResuslt = sum(num1, num2);
-
-  console.log(result);
-  console.log(stringResuslt);
+  const numResult = sum(num1, num2);
+  numResults.push(numResult as number);
+  const stringResult = sum(num1, num2);
+  textResults.push(stringResult as string);
+  
+  console.log(numResults);
+  console.log(textResults);
+  
+  printResult({val: numResult as number, timestamp: new Date()})
 });
 
 //console.log(sum(1, 6));
