256d2d624cc8dd73702b3848da0ee222ea985ecc
Martim Lima <martim.d.lima@protonmail.com>
2020-09-26 17:53:49 +0100

    Nodejs & Typescript
    - Adding REST Routes with TypeScript

--- nodejs-and-typescript/node-ts/routes/todos.ts
+++ nodejs-and-typescript/node-ts/routes/todos.ts
@@ -1,11 +1,26 @@
-import { Router } from 'express';
+import { Router } from "express";
+import { Todo } from "../models/todo";
 
-let todos = [];
+type RequestBody = { text: string };
+
+let todos: Todo[] = [];
 
 const router = Router();
 
-router.get('/', (req, res, next) => {
+router.get("/", (req, res, next) => {
   res.status(200).json({ todos: todos });
+});
+
+router.post("/todo", (req, res, next) => {
+  const body = req.body as RequestBody;
+  const newTodo: Todo = {
+    id: new Date().toISOString(),
+    text: body.text,
+  };
+
+  todos.push(newTodo);
+
+  res.status(201).json({ message: "Added Todo", todo: newTodo, todos: todos });
 });
 
 export default router;
