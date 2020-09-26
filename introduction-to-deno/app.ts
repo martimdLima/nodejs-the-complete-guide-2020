405fd349a27a40475e8461414e325272f9e88a2c
Martim Lima <martim.d.lima@protonmail.com>
2020-09-26 20:11:38 +0100

    Introduction to Deno
    - Using the Standard Library

--- introduction-to-deno/app.ts
+++ introduction-to-deno/app.ts
@@ -1,21 +1,9 @@
-const text = `
-Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-Ut morbi tincidunt augue interdum. Morbi tincidunt ornare massa eget egestas purus. 
-Eget nullam non nisi est sit amet facilisis magna etiam. Fermentum odio eu feugiat pretium. In cursus turpis massa tincidunt dui ut. 
-Nisl nisi scelerisque eu ultrices vitae auctor eu augue. 
-Volutpat consequat mauris nunc congue nisi vitae suscipit tellus mauris. 
-Porta non pulvinar neque laoreet suspendisse. Pretium nibh ipsum consequat nisl vel pretium. 
-Ut faucibus pulvinar elementum integer enim neque. Lorem ipsum dolor sit amet consectetur adipiscing elit. 
-Non pulvinar neque laoreet suspendisse interdum consectetur libero id. Arcu non sodales neque sodales ut.
-`;
+import { serve } from "https://deno.land/std/http/server.ts";
 
-const encoder = new TextEncoder();
-const data = encoder.encode(text);
+const server = serve({ port: 3000 });
 
-Deno.writeFile("loremipsum.txt", data)
-  .then(() => {
-    console.log("wrote to file successfully!");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+console.log("http://localhost:3000/");
+
+for await (const req of server) {
+  req.respond({ body: "Dummy Server is up and running!\n" });
+}
