2b5123125e469e74c0d089886e018ae2cf4ed4a0
Martim Lima <martim.d.lima@protonmail.com>
2020-09-26 16:49:35 +0100

    Nodejs & Typescript
    - Type Inference & Type Casting

--- nodejs-and-typescript/app.js
+++ nodejs-and-typescript/app.js
@@ -1,5 +1,13 @@
+var num1Element = document.getElementById('num1');
+var num2Element = document.getElementById('num2');
+var buttonElement = document.querySelector('button');
 function sum(num1, num2) {
     return num1 + num2;
 }
-console.log(sum(1, 6));
-console.log(sum('1', '6'));
+buttonElement.addEventListener('click', function () {
+    var num1 = +num1Element.value;
+    var num2 = +num2Element.value;
+    var result = sum(num1, num2);
+    console.log(result);
+});
+//console.log(sum(1, 6));
--- nodejs-and-typescript/app.ts
+++ nodejs-and-typescript/app.ts
@@ -1,7 +1,16 @@
-function sum(num1, num2) {
+const num1Element = document.getElementById('num1') as HTMLInputElement;
+const num2Element = document.getElementById('num2') as HTMLInputElement;
+const buttonElement = document.querySelector('button');
+
+function sum(num1: number, num2: number) {
   return num1 + num2;
 }
 
-console.log(sum(1, 6));
+buttonElement.addEventListener('click', () => {
+  const num1 = +num1Element.value;
+  const num2 = +num2Element.value;
+  const result = sum(num1, num2);
+  console.log(result);
+});
 
-console.log(sum('1', '6'));
+//console.log(sum(1, 6));
