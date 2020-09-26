92754dbeea17dadb1ad83beca1737a10d8beeaa0
Martim Lima <martim.d.lima@protonmail.com>
2020-09-26 17:55:46 +0100

    Nodejs & Typescript
    - Finishing the REST Routes

--- nodejs-and-typescript/node-ts/routes/todos.ts
+++ nodejs-and-typescript/node-ts/routes/todos.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { Todo } from "../models/todo";
 
 type RequestBody = { text: string };
+type RequestParams = { todoId: string };
 
 let todos: Todo[] = [];
 
@@ -22,5 +23,24 @@ router.post("/todo", (req, res, next) => {
 
   res.status(201).json({ message: "Added Todo", todo: newTodo, todos: todos });
 });
+
+router.put('/todo/:todoId', (req, res, next) => {
+  const params = req.params as RequestParams;
+  const tid = params.todoId;
+  const body = req.body as RequestBody;
+  const todoIndex = todos.findIndex((todoItem) => todoItem.id === tid);
+  if (todoIndex >= 0) {
+    todos[todoIndex] = { id: todos[todoIndex].id, text: body.text };
+    return res.status(200).json({ message: 'Updated todo', todos: todos });
+  }
+  res.status(404).json({ message: 'Could not find todo for this id.' });
+});
+
+router.delete('/todo/:todoId', (req, res, next) => {
+  const params = req.params as RequestParams;
+  todos = todos.filter((todoItem) => todoItem.id !== params.todoId);
+  res.status(200).json({ message: 'Deleted todo', todos: todos });
+});
+
 
 export default router;
